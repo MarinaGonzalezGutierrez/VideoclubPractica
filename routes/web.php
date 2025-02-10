14afdcbf1628e7779c2d2f320a7dedd5548f29d8
Marina González <marinagogu4@gmail.com>
2025-02-10 13:49:39 +0100

    practica videoclub repo

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+//EJERCICIO 2 - DEFINICION DE LAS RUTAS
+
+Route::get('/', function () {
+//    return view('welcome');
+echo 'Pantalla principal';
+});
+
+Route::get('login',function() {
+    return 'Login usuario';
+});
+
+Route::get('logout',function() {
+    return 'Logout usuario';
+});
+
+Route::get('catalog',function() {
+    return 'Listado peliculas';
+});
+
+Route::get('catalog/show/{id}',function($id){
+    return 'Vista detalle pelicula :'.$id;
+    //rellenar con el id
+});
+
+Route::get('catalog/create',function(){
+    return 'Añadir pelicula :';
+    
+});
+
+
+Route::get('catalog/edit/{id}',function($id){
+    return 'Modificar pelicula :'.$id;
+    
+});
+
