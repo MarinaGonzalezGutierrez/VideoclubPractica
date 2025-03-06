5fb1c5438e4c8dd1ec40730ed2631a173c575058
Marina González <marinagogu4@gmail.com>
2025-03-06 14:25:46 +0100

    guardar

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -1,12 +1,3 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pantalla principal</title>
-</head>
-<body>
 @extends('layouts.master')
 @section('content')<div class="row">
 @foreach( $arrayPeliculas as $key => $pelicula )
@@ -19,6 +10,4 @@
 </a>
 </div> @endforeach
 </div>
-
-</body>
-</html>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,5 @@ Route::get('catalog', [CatalogController::class, 'getIndex']);
 Route::get('catalog/show/{id}', [CatalogController::class, 'getShow']);
 Route::get('catalog/create', [CatalogController::class, 'getCreate']);
 Route::get('catalog/edit/{id}', [CatalogController::class, 'getEdit']);
+
+
