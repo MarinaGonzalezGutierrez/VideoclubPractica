bcc9f6d288016a43873b4dc5f2f1db6fc68e52d9
Marina González <marinagogu4@gmail.com>
2025-03-19 14:22:07 +0100

    otro

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -6,18 +6,17 @@
 </head>
 <body>
 @extends('layouts.master')
-@section('content')
-<div class="row">
+@section('content')<div class="row">
 <div class="col-sm-4">
-<img src="{{$pelicula->poster}}" style="height:200px"/>
+<img src="{{$arrayPeliculas[$id]['poster']}}" style="height:200px"/>
 </div>
 <div class="col-sm-8">
-<h1>{{$pelicula->title}}</h1>
-<h3>Año: {{$pelicula->year}}</h3>
-<h4>Director: {{$pelicula->director}}</h4>
-<p><strong>Resumen:</strong> {{$pelicula->synopsis}}</p>
+<h1>{{$arrayPeliculas[$id]['title']}}</h1>
+<h3>Año: {{$arrayPeliculas[$id]['year']}}</h3>
+<h4>Director: {{$arrayPeliculas[$id]['director']}}</h4>
+<p><strong>Resumen:</strong> {{$arrayPeliculas[$id]['synopsis']}}</p>
 <p><strong>Estado:</strong>
-@if($pelicula->rented)
+@if($arrayPeliculas[$id]['rented'])
 Película actualmente alquilada
 <br>
 <a href="#" class="btn btn-danger">Devolver película</a>
@@ -26,10 +25,10 @@ Película disponible
 <br>
 <a href="#" class="btn btn-primary">Alquilar película</a>
 @endif
-<a href="{{ url('/catalog/edit/' . $pelicula->id ) }}" class="btn btn-warning">Editar película</a>
+<a href="{{ url('/catalog/edit/' . $id ) }}" class="btn btn-warning">Editar película</a>
 <a href="{{ url('/catalog') }}" class="btn btn-outline-secondary">Volver al listado</a>
 </p>
-
+ 
 </div>
 </div>
 @stop
